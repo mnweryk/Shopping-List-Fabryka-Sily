964c6eea1d9240faf97896d602a5c7b94125d225
mnweryk <mnweryk@gmail.com>
2020-08-23 17:39:29 +0200

    return output file

--- calories_calculator.py
+++ calories_calculator.py
@@ -1,10 +1,12 @@
 import pdfplumber
-import pprint
 import re
+import os
+import json
 
 class PDFConverter():
     def __init__(self, path):
         self.source_path = path
+        self.product_list = self.categorize_list()
 
     def get_plain_text(self):
         pdf = pdfplumber.open(self.source_path)
@@ -14,9 +16,9 @@ class PDFConverter():
         pdf.close()
         return plain_text
 
-    def categorize_list(self, text):
+    def categorize_list(self):
+        text = self.get_plain_text().split('\n')
         product_list = dict()
-        text = text.split('\n')
 
         category = ''
         counter = 0
@@ -30,7 +32,6 @@ class PDFConverter():
                 counter += 1
                 entry = self.create_entry(element)
                 product_list[category].append(entry)
-        pprint.pprint(product_list)
         return product_list
 
     def create_entry(self, product):
@@ -40,27 +41,49 @@ class PDFConverter():
         match_two = re.match(match_two_numbers, product)
         element = dict()
         if match_one:
-            name = match_one.group(1)
-            element[name] = {}
-            element[name]['weight'] = match_one.group(2)
-            element[name]['unit'] = match_one.group(3)
+            element['name'] = match_one.group(1)
+            element['weight'] = match_one.group(2)
+            element['unit'] = match_one.group(3)
         elif match_two:
-            name = match_two.group(1)
-            element[name] = {}
-            element[name]['quantity'] = match_two.group(2)
-            element[name]['quantity_unit'] = match_two.group(3)
-            element[name]['weight'] = match_two.group(4)
-            element[name]['unit'] = match_two.group(5)
+            element['name'] = match_two.group(1)
+            element['quantity'] = match_two.group(2)
+            element['quantity_unit'] = match_two.group(3)
+            element['weight'] = match_two.group(4)
+            element['unit'] = match_two.group(5)
         else:
-            raise Exception("{} didnt match ".format(product))
+            raise Exception("{} didn't match ".format(product))
         return element
 
+class DataExporter():
+    def __init__(self, path, data, factor):
+        self.path = path
+        self.data = data
+        self.factor = factor
+        self.name = 'shopping_list.txt'
+        self.write()
+
+    def refactor(self):
+        text = ''
+        for key, value in self.data.items():
+            text += "\n{}\n".format(key)
+            for item in value:
+                element = "{} | {}{}".format(item.get('name'), float(item.get('weight'))*1.7, item.get('unit'))
+                if item.get('quantity'):
+                    element += ' | {} {}'.format(float(item.get('quantity', 0))*1.7, item.get('quantity_unit'))
+                text += '{}\n'.format(element)
+        return text
+
+    def write(self):
+        text = self.refactor()
+        with open(os.path.join(self.path, self.name), 'w', encoding='utf-8') as file:
+            file.write(text)
 
 
+factor = 1.7
 
-source_path = 'source/shopping_list.pdf'
-pdfConverter = PDFConverter(source_path)
-text = pdfConverter.get_plain_text()
-shopping_list = pdfConverter.categorize_list(text)
+SOURCE_PATH = 'source/shopping_list.pdf'
+OUTPUT_PATH = 'output'
+pdfConverter = PDFConverter(SOURCE_PATH)
 
-
+dataExporter = DataExporter(OUTPUT_PATH, pdfConverter.product_list, factor)
+# print(pdfConverter.product_list)
